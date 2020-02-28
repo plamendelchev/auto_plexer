043d9665b15ceab3f7cda278ff1dc164268673cf
Plamen Delchev <plamen.delchev@gmail.com>
2020-02-28 20:38:23 +0200

    Added usage() as well as getopts

--- auto_plex.sh
+++ auto_plex.sh
@@ -1,39 +1,86 @@
 #!/usr/bin/env bash
 
+## Plamen Delchev 28.02.2020
+
 set -o errexit
 set -o pipefail
 set -o nounset
 
-## Plamen Delchev 28.02.2020
+usage() {
+	cat << EOF
 
-## Usage
-# Main ~> auto_plex user hostname.com
-# Status ~> auto_plex -s
-# Cancel ~> auto_plex -c
-##
+Auto Plexer
 
-## Main
-#	+ Backup original ssh_config 
-#	+ Generate multiplex config
-#	+ Append multiplex config to ssh_config
-##
+Usage: 
+	auto_plex <hostname>
+	auto_plex -s <hostname>
+	auto_plex -c <hostname>
+
+Options:
+	-s Show SSH multiplex connection status
+	-c Cancel SSH multiplex connection
+
+EOF
+}
 
 # Constants
 readonly SSH_CONFIG='/Users/plamen.delchev/.ssh/config'
-readonly SSH_CONTROL_PATH='/tmp/transfer-%r@%h.local.sock'
+readonly SSH_CONTROL_PATH='/tmp/transfer-%h.local.sock'
 readonly SSH_CONTROL_MASTER='auto'
-readonly SSH_CONTROL_PERSIST='10m'
+readonly SSH_CONTROL_PERSIST='20m'
+
+while getopts ':s:c:' flag; do
+	case "${flag}" in
+		s)
+			if ! grep -q "${OPTARG}" "${SSH_CONFIG}"; then
+				echo "${OPTARG} is not present in ${SSH_CONFIG}" 1>&2
+				exit 1
+			fi
+
+			ssh -O check "${OPTARG}"
+			exit 0
+			;;
+		c) 
+			if ! grep -q "${OPTARG}" "${SSH_CONFIG}"; then
+				echo "${OPTARG} is not present in ${SSH_CONFIG}" 1>&2
+				exit 1
+			fi
+
+			ssh -O exit "${OPTARG}"
+			cat "${SSH_CONFIG}.bkp" > "${SSH_CONFIG}" && rm -f "${SSH_CONFIG}.bkp"
+			echo "${SSH_CONFIG} reverted from backup"
+
+			exit 0
+			;;
+		:)
+			echo "-${OPTARG} must be followed by an argument we" 1>&2
+			exit 1
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
+
+if (( $# == 0 )); then
+	usage
+	exit 1
+fi
 
 # Functions
 backup_ssh_config() {
-	cp -f "${SSH_CONFIG}" "${SSH_CONFIG}.bkp"
+	echo -e "Backing up ${SSH_CONFIG} ... "
+	if ! cp -f "${SSH_CONFIG}" "${SSH_CONFIG}.bkp"; then
+		echo "Unable to backup ${SSH_CONFIG}"
+		exit 1
+	fi
 }
 
 generate_multiplex_config() {
-	declare -r USERNAME="$1"
-	declare -r HOSTNAME="$2"
+	declare -r HOSTNAME="$1"
 
-	readonly MULTIPLEX_CONFIG="Host transfer-${HOSTNAME}
+	readonly MULTIPLEX_CONFIG="Host ${HOSTNAME}
 			HostName ${HOSTNAME}
 			ControlPath ${SSH_CONTROL_PATH}
 			ControlMaster ${SSH_CONTROL_MASTER}
@@ -42,21 +89,19 @@ generate_multiplex_config() {
 }
 
 append_multiplex_config() {
-	echo "${MULTIPLEX_CONFIG}" >> "${SSH_CONFIG}"
-}
-
-establish_master_connection() {
-	ssh "${USERNAME}"@"${HOSTNAME}"
-	if (( $? != 0 )); then
-		echo "Unable to connected to ${USERNAME}@${HOSTNAME}"
+	echo -e "Adding multiplex config into ${SSH_CONFIG} ... "
+	if ! echo "${MULTIPLEX_CONFIG}" >> "${SSH_CONFIG}"; then
+		echo 'Unable to add the configuration ¯\_(ツ)_/¯'
 		exit 1
 	fi
 }
 
 main() {
 	backup_ssh_config
-	generate_multiplex_config "$@"
+	generate_multiplex_config "$1"
 	append_multiplex_config
+
+	echo 'Done!'
 }
 
 main "$@"
